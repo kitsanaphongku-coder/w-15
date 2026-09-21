a47d987cd1c0250359f17afb5f4ee962994c1248
kitsanaphongku-coder <kitsanaphong.ku@rmuti.ac.th>
2026-09-21 16:19:33 +0700

    w-15

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -442,6 +442,15 @@
             'aliases' => array(),
             'dev_requirement' => true,
         ),
+        'laravel/sanctum' => array(
+            'pretty_version' => 'v4.3.3',
+            'version' => '4.3.3.0',
+            'reference' => 'fee27a573d1a013af3721d86153a65e0b11927e6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laravel/sanctum',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'laravel/serializable-closure' => array(
             'pretty_version' => 'v2.0.13',
             'version' => '2.0.13.0',
